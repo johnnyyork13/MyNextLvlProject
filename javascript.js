713b42cce22c56730ca97ab6e0146446b76fc367
johnnyyork13 <johnnyyork13@hotmail.com>
2023-01-31 18:16:27 -0500

    add responsive layout to header and mobile version

--- javascript.js
+++ javascript.js
@@ -1,21 +1,74 @@
 const navButton = document.getElementsByClassName('navButton');
 const navDrop = document.getElementsByClassName('navDrop');
 
-function dropDown() {
+function dropDownActivate() {
     for (i = 0; i < navButton.length; i++)
     {
         let e = navButton[i];
+        let n = navDrop[i];
         e.addEventListener('mouseenter', function(){
-            console.log(e.parentNode.childNodes);
             e.parentNode.childNodes[3].style.visibility = 'visible';
-            e.parentNode.childNodes[3].style.height = "auto";
         })
         e.addEventListener('mouseleave', function(){
-            e.parentNode.childNodes[3].style.height = "0px";
             e.parentNode.childNodes[3].style.visibility = 'hidden';
+        })
+        n.addEventListener('mouseenter', function(){
+            n.parentNode.childNodes[3].style.visibility = 'visible';
+        })
+        n.addEventListener('mouseleave', function(){
+            n.parentNode.childNodes[3].style.visibility = 'hidden';
         })
     }
 }
 
+dropDownActivate();
 
-dropDown();
+//MOBILE MENU DROPDOWN
+const mobileHeader = document.getElementsByClassName('mobileMenuHeader');
+const hamburger = document.getElementById('hamburgerMenu');
+const mobileMenuContainer = document.getElementById('mobileMenuContainer');
+
+function mobileDropDownActivate(){
+    hamburger.addEventListener('mouseenter', function(){
+        mobileMenuContainer.style.visibility = 'visible'
+    })
+    hamburger.addEventListener('mouseleave', function(){
+        mobileMenuContainer.style.visibility = 'hidden';
+        cleanMobileDropDown();
+    })
+    mobileMenuContainer.addEventListener('mouseenter', function(){
+        mobileMenuContainer.style.visibility = 'visible';
+    })
+    mobileMenuContainer.addEventListener('mouseleave', function(){
+        mobileMenuContainer.style.visibility = 'hidden';
+        cleanMobileDropDown();
+    })
+
+    for (i = 0; i < mobileHeader.length; i++) {
+        let e = mobileHeader[i];
+        console.log(e.parentNode.parentNode.childNodes[3]);
+        e.addEventListener('click', function(){
+            cleanMobileDropDown();
+            e.parentNode.childNodes[3].style.visibility = 'visible';
+            e.parentNode.childNodes[3].style.position = 'relative';
+        })
+    }
+}
+
+function cleanMobileDropDown() {
+    for (i = 0; i < mobileHeader.length; i++) {
+        let e = mobileHeader[i];
+        e.parentNode.parentNode.childNodes[1].childNodes[3].style.visibility = 'hidden';
+            e.parentNode.parentNode.childNodes[1].childNodes[3].style.position = 'absolute';
+            e.parentNode.parentNode.childNodes[3].childNodes[3].style.visibility = 'hidden';
+            e.parentNode.parentNode.childNodes[3].childNodes[3].style.position = 'absolute';
+            e.parentNode.parentNode.childNodes[5].childNodes[3].style.visibility = 'hidden';
+            e.parentNode.parentNode.childNodes[5].childNodes[3].style.position = 'absolute';
+            e.parentNode.parentNode.childNodes[7].childNodes[3].style.visibility = 'hidden';
+            e.parentNode.parentNode.childNodes[7].childNodes[3].style.position = 'absolute';
+            e.parentNode.parentNode.childNodes[9].childNodes[3].style.visibility = 'hidden';
+            e.parentNode.parentNode.childNodes[9].childNodes[3].style.position = 'absolute';
+    }
+}
+
+mobileDropDownActivate();
